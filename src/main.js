cb3d3d892ae967d8de78717c620d04b9a2ccad7a
skrmaster <zyskr@qq.com>
2021-05-24 14:55:19 +0800

    handle scroll to top

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.css"
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import {
+	BootstrapVue,
+	BootstrapVueIcons
+} from 'bootstrap-vue'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
@@ -11,22 +14,23 @@ Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 router.beforeResolve((to, from, next) => {
-    if (to.matched.some(record => record.name)) {
-        next()
-    } else {
+	if (to.matched.some(record => record.name)) {
+		next()
+	} else {
 		next('/')
-    }
+	}
 })
 
 router.beforeEach((to, from, next) => {
-  /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    document.title = to.meta.title
-  }
-  next()
+	/* 路由发生变化修改页面title */
+	if (to.meta.title) {
+		document.title = to.meta.title
+		window.scrollTo(0, 0)
+	}
+	next()
 })
 
 new Vue({
-  router,
-  render: h => h(App)
+	router,
+	render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -70,7 +70,6 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
